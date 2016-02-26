ab4428a3bec2e098fe535d0f432222446008e03c
melvinzhang <melvinzhang@gmail.com>
2016-02-26 10:06:36 +0800

    change functions into members of Machine

--- turing-machine.cpp
+++ turing-machine.cpp
@@ -43,120 +43,118 @@ struct Action {
 
 struct Machine {
     int tp = 0;
-    string start;
+    string curr;
     map< Configuration, Action > program;
     map< int, char > tape;
+
+    void load_program() {
+        cin.sync_with_stdio(false);
+        string line;
+        while (getline(cin, line)) {
+            // skip empty lines
+            if (line.length() == 0) {
+                continue;
+            }
+
+            // lines begining with # are comments
+            if (line.at(0) == '#') {
+                continue;
+            }
+
+            istringstream iss(line);
+            string state, symbol, ops, fstate;
+            iss >> state >> symbol >> ops >> fstate;
+
+            if (curr.length() == 0) {
+                curr = state;
+            }
+
+            Configuration c = {state, symbol.at(0)};
+            Action a = {ops, fstate};
+            program[c] = a;
+        }
+    }
+
+    void print_program() {
+        for (auto it = program.begin(); it != program.end(); it++) {
+            cout << it->first.state << " ";
+            cout << it->first.symbol << " ";
+            cout << it->second.ops << " ";
+            cout << it->second.state << endl;
+        }
+    }
+
+    void print_tape() {
+        cout << "tape: ";
+        for (auto it = tape.begin(); it != tape.end(); it++) {
+            cout << it->second;
+        }
+        cout << endl;
+    }
+
+    char read_tape() {
+        if (tape.count(tp)) {
+            return tape[tp];
+        } else {
+            return '~';
+        }
+    }
+
+    int perform_ops(string ops) {
+        for (int i = 0; i < ops.length(); i++) {
+            char op = ops.at(i);
+            switch (op) {
+                case 'R':
+                    tp++;
+                    break;
+                case 'L':
+                    tp--;
+                    break;
+                case 'E':
+                    tape[tp] = '~';
+                    break;
+                case 'P':
+                    i++;
+                    tape[tp] = ops.at(i);
+                case ',':
+                    break;
+                default:
+                    cout << "unknown op: " << op << endl;
+                    exit(1);
+            }
+        }
+        return tp;
+    }
+
+    void eval(int max) {
+        int cnt = 0;
+        while (cnt < max) {
+            Configuration c = {curr, read_tape()};
+            if (program.count(c) == 0) {
+                c = {curr, '*'};
+            }
+            if (program.count(c) == 0) {
+                break;
+            }
+            Action a = program[c];
+            tp = perform_ops(a.ops);
+            curr = a.state;
+
+            cnt++;
+            print_config(c);
+            print_tape();
+        }
+    }
+
+    void print_config(Configuration c) {
+        cout << "conf: " << c.state << " " << c.symbol << endl;
+    }
 };
-    
-void load_program(Machine &m) {
-    cin.sync_with_stdio(false);
-    string line;
-    while (getline(cin, line)) {
-        // skip empty lines
-        if (line.length() == 0) {
-            continue;
-        }
-        
-        // lines begining with # are comments
-        if (line.at(0) == '#') {
-            continue;
-        }
-
-        istringstream iss(line);
-        string state, symbol, ops, fstate;
-        iss >> state >> symbol >> ops >> fstate;
-        
-        if (m.start.length() == 0) {
-            m.start = state;
-        }
-        
-        Configuration c = {state, symbol.at(0)};
-        Action a = {ops, fstate};
-        m.program[c] = a;
-    }
-}
-
-void print_program(const Machine &m) {
-    for (auto it=m.program.begin(); it != m.program.end(); it++) {
-        cout << it->first.state << " ";
-        cout << it->first.symbol << " ";
-        cout << it->second.ops << " ";
-        cout << it->second.state << endl;
-    }
-}
-
-void print_tape(const Machine &m) {
-    cout << "tape: ";
-    for (auto it=m.tape.begin(); it != m.tape.end(); it++) {
-        cout << it->second;
-    }
-    cout << endl;
-}
-
-void print_config(Configuration c) {
-    cout << "conf: " << c.state << " " << c.symbol << endl;
-}
-
-char read_tape(Machine &m, int tp) {
-    if (m.tape.count(tp)) {
-        return m.tape[tp];
-    } else {
-        return '~';
-    }
-}
-
-int perform_ops(Machine &m, int tp, string ops) {
-    for (int i = 0; i < ops.length(); i++) {
-        char op = ops.at(i);
-        switch (op) {
-            case 'R':
-                tp++;
-                break;
-            case 'L':
-                tp--;
-                break;
-            case 'E':
-                m.tape[tp] = '~';
-                break;
-            case 'P':
-                i++;
-                m.tape[tp] = ops.at(i);
-            case ',':
-                break;
-            default:
-                cout << "unknown op: " << op << endl;
-                exit(1);
-        }
-    }
-    return tp;
-}
-
-void eval(Machine &m, int max) {
-    string curr = m.start;
-    int cnt = 0;
-    while (cnt < max) {
-        cnt++;
-        char s = read_tape(m, m.tp);
-        Configuration c = {curr, s};
-        if (m.program.count(c) == 0) {
-            c = {curr, '*'};
-        }
-        if (m.program.count(c) == 0) {
-            break;
-        }
-        Action a = m.program[c];
-        m.tp = perform_ops(m, m.tp, a.ops);
-        curr = a.state;
-        print_config(c);
-        print_tape(m);
-    }
-}
 
 int main(int argc, char *argv[]) {
     Machine m;
-    load_program(m);
-    print_program(m);
-    eval(m, 1000);
-    print_tape(m);
+    m.load_program();
+    m.print_program();
+    m.eval(1000);
     return 0;
 }
